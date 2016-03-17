c893501bdbad88f99f07aafc2b45042cbaca536d
Hasse Ramlev Hansen <hasse@ramlev.dk>
2016-03-17 08:21:56 +0100

    Added PHPUnit tests
    * Testing the AeroGearPush client
    * Testing the createApplicationRequest

---
+++ tests/AeroGearPush/Tests/AeroGearPushTest.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * This file is part of the AeroGearPush package.
+ *
+ * (c) NAPP <http://napp.dk>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace AeroGearPush\Tests;
+
+use Napp\AeroGearPush;
+use Napp\Request\createApplicationRequest;
+
+/**
+ * Class AeroGearPushTest
+ *
+ * @package AeroGearPush\Tests
+ * @author  Hasse Ramlev Hansen <hasse@ramlev.dk>
+ */
+class AeroGearPushTest extends \PHPUnit_Framework_TestCase
+{
+    public function testAeroGearPushClient()
+    {
+        $client = new AeroGearPush('https://url.com/endpoint', [
+            'verifySSL' => true,
+            'array' => [
+                'key1' => 1,
+                'key2' => 2,
+            ],
+        ]);
+
+        $this->assertEquals('https://url.com/endpoint', $client->serverUrl);
+        $this->assertEquals(true, $client->verifySSL);
+        $this->assertEquals(1, $client->array['key1']);
+    }
+
+    public function testCreateApplicationRequest()
+    {
+        $request = new createApplicationRequest();
+
+        $this->assertEquals('applications', $request->endpoint);
+        $this->assertEquals('POST', $request->method);
+    }
+}
